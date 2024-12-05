9c965d65829f2c721e8cdf05d70dadf14c937b20
Ljiangyu <104154611+Ljiangyu@users.noreply.github.com>
2024-12-05 17:58:01 +0800

    add:项目初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+// 定义应用的全局配置
+module.exports = {
+
+  title: 'Vue Admin Template', // 网站标题设置
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 是否显示侧边栏的logo
+}
