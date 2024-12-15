286480477d749d5e103fae0529342807f573cb3d
Ljiangyu <104154611+Ljiangyu@users.noreply.github.com>
2024-12-15 16:39:32 +0800

    ✨ feat(logo): update
    大小图切换

--- src/settings.js
+++ src/settings.js
@@ -1,17 +1,17 @@
 // 定义应用的全局配置
 module.exports = {
 
-  title: 'Vue Admin Template', // 网站标题设置
+    title: 'Vue Admin Template', // 网站标题设置
 
-  /**
-   * @type {boolean} true | false
-   * @description Whether fix the header
-   */
-  fixedHeader: false, // 固定头部
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    fixedHeader: false, // 固定头部
 
-  /**
-   * @type {boolean} true | false
-   * @description Whether show the logo in sidebar
-   */
-  sidebarLogo: false // 是否显示侧边栏的logo
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    sidebarLogo: true // 是否显示侧边栏的logo
 }
