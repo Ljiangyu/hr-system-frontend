130bde07dd7a6e24e9eaf8267342d7e3f1ed3bd4
Ljiangyu <104154611+Ljiangyu@users.noreply.github.com>
2025-01-18 22:23:06 +0800

    添加部门弹窗

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,56 +2,56 @@ import { getToken, removeToken, setToken } from '@/utils/auth'
 import { login, getUserInfo } from '@/api/user'
 
 const state = {
-    token: getToken(),
-    userinfo: {
+  token: getToken(),
+  userinfo: {
 
-    }
+  }
 }
 const mutations = {
-    setToken(state, token) {
-        console.log("setToken")
-        console.log(token)
-        state.token = token
-        setToken(token)
-    },
-    removeToken(state) {
-        state.token = null
-        removeToken()
-    },
-    setUserInfo(state, data) {
-        console.log("setUserInfo")
-        console.log(data)
-        state.userinfo = data
-        console.log("============setUserInfo=============")
-        console.log(state.userinfo)
-        console.log("============end_setUserInfo=============")
-    }
+  setToken(state, token) {
+    console.log('setToken')
+    console.log(token)
+    state.token = token
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  },
+  setUserInfo(state, data) {
+    console.log('setUserInfo')
+    console.log(data)
+    state.userinfo = data
+    console.log('============setUserInfo=============')
+    console.log(state.userinfo)
+    console.log('============end_setUserInfo=============')
+  }
 }
 
 const actions = {
-    async login(context, data) {
-        const res = await login(data)
-        console.log('========login=====')
-        console.log(res)
-        console.log(res.data)
-        context.commit('setToken', res.data.data)
-    },
-    async getInfo(context) {
-        console.log("getInfo")
-        const res = await getUserInfo()
-        console.log('========res=====')
-        console.log(res)
-        context.commit('setUserInfo', res.data.data)
-    },
-    logout(context) {
-        context.commit('removeToken')
-        context.commit('setUserInfo', {})
-    }
+  async login(context, data) {
+    const res = await login(data)
+    console.log('========login=====')
+    console.log(res)
+    console.log(res.data)
+    context.commit('setToken', res.data.data)
+  },
+  async getInfo(context) {
+    console.log('getInfo')
+    const res = await getUserInfo()
+    console.log('========res=====')
+    console.log(res)
+    context.commit('setUserInfo', res.data.data)
+  },
+  logout(context) {
+    context.commit('removeToken')
+    context.commit('setUserInfo', {})
+  }
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions
+  namespaced: true,
+  state,
+  mutations,
+  actions
 }
--- src/views/department/component/add-dept.vue
+++ src/views/department/component/add-dept.vue
@@ -1,6 +1,40 @@
 <template>
-  <el-dialog title="新增部门" @close="btnCancel" :visible="showDialog">
-    <!-- 显示弹窗类型 -->
+  <el-dialog title="新增部门" :visible="showDialog" @close="btnCancel">
+    <!-- 使用 el-form -->
+    <el-form label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input
+          placeholder="请输入部门名称"
+          style="width: 80%"
+          size="mini"
+        />
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input
+          placeholder="请输入部门编码"
+          style="width: 80%"
+          size="mini"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col :span="12">
+            <el-button
+              type="primary"
+              size="mini"
+              @click="btnCancel"
+            >取消</el-button>
+          </el-col>
+          <el-col :span="12">
+            <el-button
+              type="primary"
+              size="mini"
+              @click="btnCancel"
+            >确定</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
   </el-dialog>
 </template>
 
@@ -9,15 +43,15 @@ export default {
   props: {
     showDialog: {
       type: Boolean,
-      default: false,
-    },
+      default: false
+    }
   },
   methods: {
     btnCancel() {
-      this.$emit("update:showDialog", false);
-    },
-  },
-};
+      this.$emit('update:showDialog', false)
+    }
+  }
+}
 </script>
 
 <style>
